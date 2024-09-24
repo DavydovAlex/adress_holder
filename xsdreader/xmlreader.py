b0b11894483a91b9a5a1fdfe797dc1ce227f9f6b
davam <davam@nso.ru>
2024-09-24 16:27:10 +0700

    реализовано приведение типов xml файла

---
+++ xsdreader/xmlreader.py
@@ -0,0 +1,68 @@
+from dataclasses import dataclass
+from xsdreader.builder import XsdObject, Attribute, ABoolean, ALong, AInteger, AString, ADate
+from datetime import datetime
+class DataColumn:
+
+    def __init__(self, name, value):
+        self._name = name
+        self._value = value
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        self._name = value
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, value):
+        self._value = value
+
+
+
+class DataRow:
+
+    def __init__(self, xsd_object: XsdObject =None):
+        self.columns = []
+        self._xsd_object =xsd_object
+
+    def append(self, column: DataColumn):
+        if self._xsd_object is not None:
+            column.value = self.change_value_type(column, self._xsd_object)
+        self.columns.append(column)
+
+    def change_value_type(self, column: DataColumn, xsd_object: XsdObject):
+        columns_types = xsd_object.attributes
+        type_ = None
+        for c_t in columns_types:
+            if column.name == c_t.name:
+                type_ = c_t
+                break
+        if type_ is None:
+            raise Exception("В соответствующем xsd файле отсутствует поле '{}'".format(column.name))
+        if isinstance(c_t, AString):
+            pass
+        elif isinstance(c_t, ALong):
+            return int(column.value)
+        elif isinstance(c_t, AInteger):
+            return int(column.value)
+        elif isinstance(c_t, ABoolean):
+            if isinstance(column.value, bool):
+                pass
+            elif isinstance(column.value, str):
+                return True if column.value.lower() == 'true' else False
+        elif isinstance(c_t, ADate):
+            return datetime.strptime(column.value, '%Y-%m-%d')
+        else:
+            raise Exception("Не определен тип данных")
+        return column.value
+
+
+
+
+
