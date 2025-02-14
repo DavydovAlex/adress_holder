f937465fd528c338c9c705e8fbdfc0232d78deec
davam <davam@nso.ru>
2025-02-14 16:07:42 +0700

    отказ от xsd продолжение

--- app/tables_config/table.py
+++ app/tables_config/table.py
@@ -1,20 +1,46 @@
+import dataclasses
 import os.path
 import yaml
 
 
-def _check_property_to_None( value):
+def _check_if_None(value):
     if value is None:
         raise ValueError(f'Field "{value}" cant be empty')
 
+
+
 class Column:
+
+    @dataclasses.dataclass
+    class Datatype:
+        datatype: str
+        db_datatype: str
+
+    allowed_datatypes = [
+        Datatype('string',
+                 'VARCHAR'),
+        Datatype('long',
+                 'BIGINT'),
+        Datatype('integer',
+                 'INTEGER'),
+        Datatype('boolean',
+                 'BOOLEAN'),
+        Datatype('date',
+                 'DATE'),
+    ]
 
     def __init__(self,
                  name: str,
                  data_type: str,
                  comment: str,
-                 length: int | None,
-                 primary_key: bool):
-        pass
+                 length: int | None = None,
+                 primary_key: bool | None = None):
+        self.name = name
+        self.data_type = data_type
+        self.db_datatype = self._get_db_datatype(data_type)
+        self.comment = comment
+        self.length = length
+        self.primary_key = primary_key
 
     @property
     def name(self):
@@ -22,7 +48,7 @@ class Column:
 
     @name.setter
     def name(self, value):
-        _check_property_to_None(value)
+        _check_if_None(value)
         self._name = value
 
     @property
@@ -31,7 +57,8 @@ class Column:
 
     @data_type.setter
     def data_type(self, value):
-        _check_property_to_None(value)
+        _check_if_None(value)
+        self._check_is_valid_datatype(value)
         self._data_type = value
 
     @property
@@ -39,9 +66,55 @@ class Column:
         return self._comment
 
     @comment.setter
-    def data_type(self, value):
-        _check_property_to_None(value)
+    def comment(self, value):
+        _check_if_None(value)
         self._comment = value
+
+    @property
+    def length(self):
+        return self._length
+
+    @length.setter
+    def length(self, value):
+        if self.data_type == 'string':
+            self._length = value
+        else:
+            self._length = None
+
+    @property
+    def primary_key(self):
+        return self._primary_key
+
+    @primary_key.setter
+    def primary_key(self, value):
+        if value is None:
+            self._primary_key = False
+        else:
+            self._primary_key = value
+
+    @property
+    def db_datatype(self):
+        return self._db_datatype
+
+    @db_datatype.setter
+    def db_datatype(self, value):
+        _check_if_None(value)
+        self._db_datatype = value
+
+
+
+    def _check_is_valid_datatype(self, value: str):
+        valid_datatypes = set([allowed_datatype.datatype for allowed_datatype in Column.allowed_datatypes])
+        if value not in valid_datatypes:
+            raise ValueError(
+                f'Data type "data_type" not belong to allowed types: [{', '.join(valid_datatypes)}]')
+
+    def _get_db_datatype(self, datatype):
+        self._check_is_valid_datatype(datatype)
+        for allowed_datatype in Column.allowed_datatypes:
+            if allowed_datatype.datatype == datatype:
+                return allowed_datatype.db_datatype
+
 
 class Table:
 
@@ -62,7 +135,7 @@ class Table:
 
     @source_file_pattern.setter
     def source_file_pattern(self, value):
-        _check_property_to_None(value)
+        _check_if_None(value)
         self._source_file_pattern = value
 
     @property
@@ -79,7 +152,7 @@ class Table:
 
     @description.setter
     def description(self, value):
-        _check_property_to_None(value)
+        _check_if_None(value)
         self._description = value
 
     @property
@@ -88,7 +161,7 @@ class Table:
 
     @db_tablename.setter
     def db_tablename(self, value):
-        _check_property_to_None(value)
+        _check_if_None(value)
         self._db_tablename = value
 
 
